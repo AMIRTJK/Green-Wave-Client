a260123152d0e85e3d483abe1da048f51820f2f6
AMIRTJK <jsharipovamir@gmail.com>
2024-09-27 13:54:52 +0500

    fx

--- src/data/api/sendRequestEmail.js
+++ src/data/api/sendRequestEmail.js
@@ -1,15 +1,19 @@
 // re_YMWcQ5v1_3LdUaGdaiDmcSVhv2QG7PXG2
 // http://localhost:4000/api/email
+// https://green-wave-server.onrender.com
 
 export const sendRequestEmail = async (formData) => {
   try {
-    const response = await fetch("https://green-wave.onrender.com/api/email", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
+    const response = await fetch(
+      "https://green-wave-server.onrender.com/api/email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Ошибка при отправке заявки.");
