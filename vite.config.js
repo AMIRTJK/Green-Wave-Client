c7c56a0f0d0ce224d4a47fdbdbc53880927976f2
AMIRTJK <jsharipovamir@gmail.com>
2024-09-27 13:37:41 +0500

    fx

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Green-Wave",
+  base: "/Green-Wave-Client",
 });
